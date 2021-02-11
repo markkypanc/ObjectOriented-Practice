b2d7c8ad2da73fbbdf6287e305905ba3409d019d
markkypanc <rphanchotong@gmail.com>
2021-02-11 13:19:36 +0700

    Updates codes

--- pset5/Lab_5_Problem_1.cpp
+++ pset5/Lab_5_Problem_1.cpp
@@ -23,10 +23,12 @@
 using namespace std;
 
 int** createMatrix(int numRows, int numColumns);
-void setMatrix(int **matrix, int numRows, int numColumns, char name);
+void setMatrixByIndex(int **matrix, int rowIndex, int colIndex, int value);
+int getMatrixByIndex(const int **matrix, int rowIndex, int colIndex);
 void matrixAddition(int **matrixA, int **matrixB, int **matrixC, int numRows, int numColumns);
 void matrixMultiplication(int **matrixA, int **matrixB, int **matrixC, int numRowsA, int numColumnsA, int numRowsB, int numColumnsB);
 void printMatrix(int **myArray, int row, int column, char name);
+void deleteMatrix(int **metrix, int numRows);
 
 int main(int argc, char* argv[]) {
     
@@ -67,22 +69,43 @@ int main(int argc, char* argv[]) {
         int **matrixA = createMatrix(numRowsA, numColumnsA);
         int **matrixB = createMatrix(numRowsB, numColumnsB);
         
-        setMatrix(matrixA, numRowsA, numColumnsA, 'A');
-        setMatrix(matrixB, numRowsB, numColumnsB, 'B');
+        int valueA;
+        for (int i = 0; i < numRowsA; i++) {
+            for (int j = 0; j < numColumnsA; j++) {
+                cout << "MatrixA[" << i << "][" << j << "]: ";
+                cin >> valueA;
+                setMatrixByIndex(matrixA, i, j, valueA);
+            }
+        }
+        
+        int valueB;
+        for (int i = 0; i < numRowsB; i++) {
+            for (int j = 0; j < numRowsB; j++) {
+                cout << "MatrixB[" << i << "][" << j << "]: ";
+                cin >> valueB;
+                setMatrixByIndex(matrixB, i, j, valueB);
+            }
+        }
+        
         
         if (decision == 1) {
             int **matrixC = createMatrix(numRowsA, numColumnsA);
             matrixAddition(matrixA, matrixB, matrixC, numRowsA, numColumnsA);
             cout << endl;
             printMatrix(matrixC, numRowsA, numColumnsA, 'C');
+            deleteMatrix(matrixC, numRowsA);
             cout << endl;
         } else {
             int **matrixC = createMatrix(numRowsA, numColumnsB);
             matrixMultiplication(matrixA, matrixB, matrixC, numRowsA, numColumnsA, numRowsB, numColumnsB);
             cout << endl;
             printMatrix(matrixC, numRowsA, numColumnsB, 'C');
+            deleteMatrix(matrixC, numRowsA);
             cout << endl;
         }
+        
+        deleteMatrix(matrixA, numRowsA);
+        deleteMatrix(matrixB, numRowsB);
     }
     
     return 0;
@@ -96,29 +119,12 @@ int** createMatrix(int numRows, int numColumns) {
     return matrix;
 }
 
-void setMatrix(int **matrix, int numRows, int numColumns, char name) {
-    for (int i = 0; i < numRows; i++) {
-        for (int j = 0; j < numColumns; j++) {
-            cout << name << "[" << i+1 << "][" << j+1 << "]: ";
-            cin >> matrix[i][j];
-        }
-    }
+void setMatrixByIndex(int **matrix, int rowIndex, int colIndex, int value) {
+    matrix[rowIndex][colIndex] = value;
 }
 
-void printMatrix(int **myArray, int row, int column, char name) {
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < column; j++) {
-            cout << name << "[" << i << "][" << j << "] " << myArray[i][j] << "\t";
-        }
-        cout << endl;
-    }
-}
-
-void freeMatrix(int **metrix, int numRows) {
-    for (int i = 0; i < numRows; i++) {
-        delete[] *(metrix + i);
-    }
-    delete[] metrix;
+int getMatrixByIndex(const int **matrix, int rowIndex, int colIndex) {
+    return matrix[rowIndex][colIndex];
 }
 
 void matrixAddition(int **matrixA, int **matrixB, int **matrixC, int numRows, int numColumns) {
@@ -147,4 +153,22 @@ void matrixMultiplication(int **matrixA, int **matrixB, int **matrixC, int numRo
             }
         }
     }
+}
+
+
+
+void printMatrix(int **myArray, int row, int column, char name) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            cout << name << "[" << i << "][" << j << "] " << myArray[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+void deleteMatrix(int **metrix, int numRows) {
+    for (int i = 0; i < numRows; i++) {
+        delete[] *(metrix + i);
+    }
+    delete[] metrix;
 }
