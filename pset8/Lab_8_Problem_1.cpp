37ba5581485c6e50bbecbac03591310530f57280
markkypanc <rphanchotong@gmail.com>
2021-03-11 14:15:15 +0700

    Debug problem1

--- pset8/Lab_8_Problem_1.cpp
+++ pset8/Lab_8_Problem_1.cpp
@@ -4,219 +4,218 @@
  Name: Rachapoom Phanchotong
  Lab No: 8
  Question No: 1
- Date: March 9, 2021
+ Date: February 23, 2021
  
  */
 
-// TODO Tasks
-// Adding copy constructors
-// overload the assignment operator
-
 #include <iostream>
+#include <vector>
 using namespace std;
-
-// MARK: - CircleClass
-
-class Circle {
-    
+class Matrix {
 private:
-    
-    double radius;
+    int *data;
+    int rows, cols;
     
 public:
-    
-    // Constructor
-    Circle(double r) {
-        this->radius = r;
+    Matrix() : rows{0}, cols{0}, data{nullptr} {}
+    Matrix(int aRows, int aCols) {
+        setMatrix(aRows, aCols);
     }
     
-    // Copy constructor
-    Circle(const Circle &circle) {
-        this->radius = circle.radius;
+    // Copy Constructor
+    Matrix(const Matrix& other) {
+        this->setMatrix(other.rows, other.cols);
+        for (int i = 0; i < other.rows; i++) {
+            for (int j = 0; j < other.cols; j++) {
+                this->setDataIndex(i, j, other.getDataIndex(i, j));
+            }
+        }
     }
     
     // Overload assignment operator
-    Circle& operator = (Circle &circle) {
-        this->radius = circle.getRadius();
-        return circle;
+    Matrix& operator=(const Matrix matrix) {
+        setMatrix(matrix.rows, matrix.cols);
+        for (int i = 0; i < matrix.rows; i++) {
+            for (int j = 0; j < matrix.cols; j++) {
+                this->setDataIndex(i, j, matrix.getDataIndex(i, j));
+            }
+        }
+        return *this;
     }
     
-    double getArea() {
-        return M_PI * pow(this->radius, 2);
+    ~Matrix() {
+        if (data) {
+            delete[] data;
+        }
     }
     
-    double getRadius() {
-        return this->radius;
+    void setMatrix(int aRows, int aCols) {
+        if (aRows > 0 && aCols > 0) {
+            data = new int[aRows * aCols];
+            rows = aRows;
+            cols = aCols;
+        }
+        else {
+            rows = 0;
+            cols = 0;
+            data = nullptr;
+        }
     }
-    
-};
-
-
-// MARK: - RectangleClass
-
-class Rectangle {
-    
-private:
-    
-    double width;
-    double height;
-    
-public:
-    
-    // Constructor
-    Rectangle(double width, double height) {
-        this->width = width;
-        this->height = height;
+    int getRows() const {
+        return rows;
     }
-    
-    // Copy constructor
-    Rectangle(const Rectangle &rectangle) {
-        this->width = rectangle.width;
-        this->height = rectangle.height;
+    int getCols() const {
+        return cols;
     }
-    
-    // Overload assignment operator
-    Rectangle& operator = (Rectangle &rectangle) {
-        this->width = rectangle.getWidth();
-        this->height = rectangle.getHeight();
-        return rectangle;
-    }
-    
-    double getArea() {
-        return this->width * this->height;
-    }
-    
-    double getWidth() {
-        return this->width;
-    }
-    
-    double getHeight() {
-        return this->height;
-    }
-};
-
-    
-// MARK: - TriangleClass
-
-class Triangle {
-
-private:
-    
-    double base;
-    double height;
-    
-public:
-    
-    // Constructor
-    Triangle(double base, double height) {
-        this->base = base;
-        this->height = height;
-    }
-    
-    // Copy constructor
-    Triangle(const Triangle &triangle) {
-        this->base = triangle.base;
-        this->height = triangle.height;
-    }
-    
-    // Overload assignment operator
-    Triangle& operator = (Triangle &triangle) {
-        this->base = triangle.getBase();
-        this->height = triangle.getHeight();
-        return triangle;
-    }
-    
-    double getArea() {
-        return (this->base * this->height) / 2.0;
-    }
-    
-    double getBase() {
-        return this->base;
-    }
-    
-    double getHeight() {
-        return this->height;
-    }
-};
-
-// MARK: - UtilityClass
-class PrintShape {
-public:
-    static void printArea(Circle circle) {
-        double radius = circle.getRadius();
-        double area = circle.getArea();
-        cout << "Area of a circle with radius of " << radius << " is " << area << endl;
-    }
-    
-    static void printArea(Rectangle rectangle) {
-        double width = rectangle.getWidth();
-        double height = rectangle.getHeight();
-        cout << "Area of a rectangle with " << width << " width and " << height << " height is ";
-        cout << width * height << endl;
-    }
-    
-    static void printArea(Triangle triangle) {
-        double base = triangle.getBase();
-        double height = triangle.getHeight();
-        double area = triangle.getArea();
-        cout << "Area of a triangle with " << base << " base length and " << height << " height is " << area << endl;
-    }
-    
-};
-
-// MARK: - MAIN
-void fillDimension(string type);
-
-int main(int argc, char* argv[]) {
-    
-    while (true) {
+    void setDataIndex(int i, int j, int val) {
+        *(data + (((i) * cols) + j)) = val;
         
-        int decision;
-        string shape[3] = {"cir", "rect", "tri"};
-        
-        cout << "1) Circle\n";
-        cout << "2) Rectangle\n";
-        cout << "3) Triangle\n";
-        cout << "0) Exit\n";
+    }
+    int getDataIndex(int i, int j) const {
+        return *(data + (((i) * cols) + j));
+    }
+    Matrix mulMatrix(const Matrix &multiplier) {
+        Matrix result;
+        int rows{getRows()}, cols{getCols()}, value;
+        if (cols == multiplier.getRows()) {
+            result.setMatrix(rows, multiplier.getCols());
+            for(int i{0}; i < rows; ++i) {
+                for(int j{0}; j < multiplier.getCols(); ++j) {
+                    value = 0;
+                    for (int k = 0; k < cols;k++) {
+                        value += getDataIndex(i, k) *
+                        multiplier.getDataIndex(k, j);
+                    }
+                    result.setDataIndex(i,j,value);
+                }
+            }
+        }
+        return result;
+    }
+    Matrix addMatrix(const Matrix &adder) {
+        Matrix result;
+        int rows{getRows()}, cols{getCols()};
+        if (adder.getRows() == rows && adder.getCols() == cols) {
+            result.setMatrix(rows, cols);
+            for(int i{0}; i < rows; i++) {
+                for (int j{0}; j < cols; j++) {
+                    result.setDataIndex(i,j,getDataIndex(i,j) + adder.getDataIndex(i,j));
+                }
+            }
+        }
+        return result;
+    }
+    
+};
+void enterMatrixRowsAndCols(const string prompt, int &numRows, int &numCols);
+void enterMatrixData(const string matrixName, Matrix &matrix);
+void printMatrix(const string matrixName, const Matrix &matrix);
+void displayResult(const Matrix &matrixA, const Matrix &matrixB, const Matrix &matrixC);
+void processMatrixAddition();
+void processMatrixMultiplication();
+
+int main() {
+    int choice{};
+    do {
+        cout << "The program to do the matrix addition or multiplication" << endl;
+        cout << "This program will ask you the information of Matrix A and B and show the result in Matrix C" << endl;
+        cout << "1.Matrix Addition\n2.Matrix Multiplication\n0.Exit\n";
         cout << "Your choice: ";
-
-        cin >> decision;
-        
-        if (decision >= 1 and decision <= 3) {
-            fillDimension(shape[decision - 1]);
-            cout << endl;
-            continue;
+        cin >> choice;
+        switch(choice) {
+            case 1:
+                processMatrixAddition();
+                break;
+            case 2:
+                processMatrixMultiplication();
+                break;
+            case 0:
+                break;
+            default:
+                cout << "Enter 0-3 only\n\n";
         }
         
-        cout << "The program has been terminated.\n";
-        exit(0);
+    } while(choice != 0);
+    cout << "Good bye\n";
+    return 0;
+}
+void processMatrixAddition() {
+    int numRowsA, numColsA;
+    int numRowsB, numColsB;
+    string errMsg {"Error! Matrix A and B must have the same dimension to perform the addition"};
+    enterMatrixRowsAndCols("Enter rows and cols for Matrix A separated By space ", numRowsA, numColsA);
+    enterMatrixRowsAndCols("Enter rows and cols for Matrix B separated By space ", numRowsB, numColsB);
+    if ((numRowsA == numRowsB) && (numColsA == numColsB)) {
+        Matrix matrixA(numRowsA, numColsA), matrixB(numRowsB, numColsB);
+        enterMatrixData("A", matrixA);
+        enterMatrixData("B", matrixB);
+        Matrix matrixC = matrixA.addMatrix(matrixB);
+        if (matrixC.getRows() != 0) {
+            displayResult(matrixA, matrixB, matrixC);
+        }
+        else {
+            cout << errMsg << endl;
+        }
+        
+    }
+    else {
+        cout << errMsg << endl;
     }
 }
 
-void fillDimension(string type) {
-    
-    double temp1, temp2;
-    
-    if (type == "cir") {
-        cout << "Enter the radius of a circle: ";
-        cin >> temp1;
-        Circle circle(temp1);
-        PrintShape::printArea(circle);
-    }
-    else if (type == "rect") {
-        cout << "Enter the width of a rectangle: ";
-        cin >> temp1;
-        cout << "Enter the height of a rectangle: ";
-        cin >> temp2;
-        Rectangle rectangle(temp1, temp2);
-        PrintShape::printArea(rectangle);
+void processMatrixMultiplication() {
+    int numRowsA, numColsA;
+    int numRowsB, numColsB;
+    string errMsg {"Error! Matrix A and B dimensions do not match to perform the multiplication"};
+    enterMatrixRowsAndCols("Enter rows and cols for Matrix A separated By space ", numRowsA, numColsA);
+    enterMatrixRowsAndCols("Enter rows and cols for Matrix B separated By space ", numRowsB, numColsB);
+    if (numColsA == numRowsB ) {
+        Matrix matrixA(numRowsA, numColsA), matrixB(numRowsB, numColsB);
+        enterMatrixData("A", matrixA);
+        enterMatrixData("B", matrixB);
+        Matrix matrixC = matrixA.mulMatrix(matrixB);
+        if (matrixC.getRows() != 0) {
+            displayResult(matrixA, matrixB, matrixC);
+        }
+        else {
+            cout << errMsg << endl;
+        }
+        
     }
     else {
-        cout << "Enter the base length of a triangle: ";
-        cin >> temp1;
-        cout << "Enter the height of a triangle: ";
-        cin >> temp2;
-        Triangle triangle(temp1, temp2);
-        PrintShape::printArea(triangle);
+        cout << errMsg << endl;
     }
+}
+void enterMatrixRowsAndCols(const string prompt, int &numRows, int &numCols) {
+    cout << prompt;
+    cin >> numRows >> numCols;
+}
+void enterMatrixData(const string matrixName, Matrix &matrix) {
+    int value;
+    for(int i{0}; i < matrix.getRows(); ++i) {
+        for(int j{0}; j < matrix.getCols(); ++j) {
+            cout << matrixName <<"[" << i+1 << "]" << "[" << j+1 << "] ";
+            cin >> value;
+            matrix.setDataIndex(i,j,value);
+        }
+    }
+}
+void printMatrix(const string matrixName, const Matrix &matrix) {
+    int value;
+    for(int i{0}; i < matrix.getRows(); ++i) {
+        for(int j{0}; j < matrix.getCols(); ++j) {
+            value = matrix.getDataIndex(i, j);
+            cout << matrixName <<"[" << i+1 << "]" << "[" << j+1 << "] " << value << " ";
+        }
+        cout << endl;
+    }
+}
+void displayResult(const Matrix &matrixA, const Matrix &matrixB, const Matrix &matrixC) {
+    cout << "-----------------------" << endl;
+    printMatrix("A", matrixA);
+    cout << "-----------------------" << endl;
+    printMatrix("B", matrixB);
+    cout << "-----------------------" << endl;
+    printMatrix("C", matrixC);
+    cout << "-----------------------" << endl;
 }
